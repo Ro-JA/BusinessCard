500101b3ab9c8c5231ed14cdf50e92ec83cbcc9f
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-02-22 17:05:06 +0900

    add res

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -10,11 +12,17 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,21 +44,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BusinessCardApp() {
-    FullNameCard(fullName = stringResource(id = R.string.my_name),
-        title = stringResource(id = R.string.title) )
+    FullNameCard(
+        fullName = stringResource(id = R.string.my_name),
+        title = stringResource(id = R.string.title)
+    )
 }
 
 @Composable
 fun FullNameCard(fullName: String, title: String, modifier: Modifier = Modifier) {
     Column(
-        modifier = Modifier.padding(all = 16.dp)
+        modifier = Modifier.padding(all = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
 
     ) {
         val image = painterResource(id = R.drawable.android_logo)
-
+        Image(painter = image, contentDescription = null)
+        Text(text = fullName, fontWeight = FontWeight.Bold, color = Color.White,
+            fontSize = 40.sp)
+        Text(text = title, color = Color.White)
 
     }
+
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
