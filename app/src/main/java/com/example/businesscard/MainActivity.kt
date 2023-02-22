81af4c0748a25a955e52b9cdc9df103ed4a6416a
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-02-22 17:58:09 +0900

    work

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.businesscard
 
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.DragInteraction
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -44,10 +45,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BusinessCardApp() {
-    FullNameCard(
-        fullName = stringResource(id = R.string.my_name),
-        title = stringResource(id = R.string.title)
-    )
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        FullNameCard(
+            fullName = stringResource(id = R.string.my_name),
+            title = stringResource(id = R.string.title)
+        )
+
+        ContactBusinessCard(
+            phoneNumber = stringResource(id = R.string.number_telephone),
+            telegram = stringResource(id = R.string.telegram),
+            email = stringResource(id = R.string.email)
+        )
+    }
 }
 
 @Composable
@@ -60,14 +76,71 @@ fun FullNameCard(fullName: String, title: String, modifier: Modifier = Modifier)
 
     ) {
         val image = painterResource(id = R.drawable.android_logo)
-        Image(painter = image, contentDescription = null)
-        Text(text = fullName, fontWeight = FontWeight.Bold, color = Color.White,
-            fontSize = 40.sp)
-        Text(text = title, color = Color.White)
+        Image(painter = image, contentDescription = null,
+        Modifier.size(200.dp))
+        Text(
+            text = fullName, fontWeight = FontWeight.Bold, color = Color.White,
+            fontSize = 40.sp
+        )
+        Text(text = title, color = Color.White, fontSize = 20.sp)
 
     }
+}
 
+@Composable
+fun ContactBusinessCard(
+    phoneNumber: String, telegram: String, email: String,
+    modifier: Modifier = Modifier
+) {
+    Column(Modifier.padding(top = dimensionResource(id = R.dimen.padding_20))) {
+        Row(Modifier.padding(top = dimensionResource(id = R.dimen.padding_8))) {
+            val imagePhone = painterResource(id = R.drawable.baseline_phone_24)
+            Image(
+                painter = imagePhone, contentDescription = null,
+                Modifier.padding(start = dimensionResource(id = R.dimen.padding_16))
+            )
+            Text(
+                text = phoneNumber,
+                Modifier.padding(start = dimensionResource(id = R.dimen.padding_4)),
+                color = Color.White
+            )
+        }
+        Row(Modifier.padding(top = dimensionResource(id = R.dimen.padding_8))) {
+            val imageShare = painterResource(id = R.drawable.baseline_share_24)
+            Image(
+                painter = imageShare, contentDescription = null,
+                Modifier.padding(
+                    start = dimensionResource(
+                        id = R.dimen.padding_16
+                    )
+                )
+            )
+            Text(
+                text = telegram,
+                Modifier.padding(start = dimensionResource(id = R.dimen.padding_4)),
+                color = Color.White
+            )
 
+        }
+        Row(Modifier.padding(top = dimensionResource(id = R.dimen.padding_8))) {
+            val imageEmail = painterResource(id = R.drawable.baseline_email_24)
+            Image(
+                painter = imageEmail, contentDescription = null,
+                Modifier.padding(
+                    start = dimensionResource(
+                        id = R.dimen.padding_16
+                    )
+                )
+            )
+            Text(
+                text = email,
+                Modifier.padding(start = dimensionResource(id = R.dimen.padding_4)),
+                color = Color.White
+
+            )
+
+        }
+    }
 }
 
 
